c795789a3a2582f8832f0e2f6d693a41b8ba98e3
Dario Villalta <dario.villalta@unitec.edu>
2018-06-29 18:03:35 -0600

    sintactico.java

--- program.asm
+++ program.asm
@@ -2,8 +2,7 @@
 msg_1:		.asciiz"ferferf"
 msg_2:		.asciiz"ferferf"
 msg_3:		.asciiz"ferferf"
-msg_4:		.asciiz"HOLA MUNDO"
-msg_5:		.asciiz"HELLO"
+msg_4:		.asciiz"HELLO"
 c:			.space '1'
 g			.WORD 0
 
@@ -15,12 +14,16 @@ suma:
 	sw $ra, -8($sp)
 	sw $s0, -12($sp)
 	move $s0, $a0
+	move $fp, $sp
+	sub $sp, $sp, 25
 	li $v0 ,4
 	la $a0, msg_1
 	syscall
 	li $v0 ,4
 	la $a0, msg_2
 	syscall
+finFunc_suma: 
+	add $sp, $sp, 25
 	lw $s0, -12($fp)
 	sw $fp, -8($sp)
 	sw $ra, -4($sp)
@@ -33,19 +36,16 @@ hola:
 	sw $s1, -16($sp)
 	move $s0, $a0
 	move $s1, $a1
+	move $fp, $sp
+	sub $sp, $sp, 32
 	li $v0 ,4
 	la $a0, msg_3
 	syscall
 	li $v0 ,4
 	la $a0, msg_4
 	syscall
-	lw $t0, hello
-	li $v0 ,4
-	move $a0, $t0
-	syscall
-	li $v0 ,4
-	la $a0, msg_5
-	syscall
+finFunc_hola: 
+	add $sp, $sp, 32
 	lw $s1, -16($fp)
 	lw $s0, -12($fp)
 	sw $fp, -8($sp)
